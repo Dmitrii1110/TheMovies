9e9d464953a2921b90c1ed2ffaca912c78fa9594
Dmitrii_IT <74114011+Dmitrii1110@users.noreply.github.com>
2022-02-15 11:07:57 +0300

    Initial Commit

---
+++ TheMovies/TheMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMoviesApp.swift
+//  TheMovies
+//
+//  Created by admin1 on 15.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct TheMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
